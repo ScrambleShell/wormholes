62c0cb0e15df5775d3ffd5261fe6b23874678d9e
Your Name <you@example.com>
2018-03-03 18:19:01 -0300

    main.go: Establishing a nice basic usage to lay the foundation for future development

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	// TODO: Use JSON to log info
 	//"encoding/json"
 	"fmt"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
+	"os"
 
 	. "github.com/hackwave/color"
 
@@ -14,38 +14,61 @@ import (
 )
 
 type Config struct {
-	Debug           bool             `json:"debug" yaml:"debug"`
-	NamecheapConfig namecheap.Config `json:"namecheap" yaml:"namecheap"`
+	Debug     bool             `json:"debug" yaml:"debug"`
+	Namecheap namecheap.Config `json:"namecheap" yaml:"namecheap"`
+}
+
+type Application struct {
+	Name           string
+	Version        string
+	Description    string
+	ConfigFilepath string
+	Config         `json:"config" yaml:"config"`
 }
 
 func main() {
-	fmt.Println(Magenta("Wormholes"), Gray(":"), Blue("Endpoint Management"))
+	// TODO: Get i18n translations and strings loading from JSON or YAML early, so its not
+	// a difficult transition later.
+	app := Application{
+		Name:           "Wormholes",
+		Version:        "0.1.0",
+		Description:    "Diverse end point management",
+		ConfigFilepath: "./config.yaml",
+	}
+
+	fmt.Println(Magenta(app.Name), Gray(":"), Blue(app.Description))
 	fmt.Println(Gray("=========================="))
-
-	// TODO: Load JSON configuration
-	yamlFile, err := ioutil.ReadFile("./config.yaml")
+	fmt.Println(Gray("[Config]"), "loading YAML configuration file: ", app.ConfigFilepath)
+	yamlFile, err := ioutil.ReadFile(app.ConfigFilepath)
 	if err != nil {
-		fmt.Println(Red("[Error]"), Gray(err))
+		fmt.Println(Red("[Fatal Error]"), err)
+		os.Exit(1)
 	}
 
-	var config Config
-	err = yaml.Unmarshal(yamlFile, &config)
+	err = yaml.Unmarshal(yamlFile, &app.Config)
 	if err != nil {
-		fmt.Println(Red("[ Error ]"), Gray(err))
+		fmt.Println(Red("[Fatal Error]"), err)
+		os.Exit(1)
 	}
+	fmt.Println(Green("[Config]"), "Configuration successfully loaded.")
+	if app.Config.Namecheap.Username != "" &&
+		app.Config.Namecheap.API.Username != "" &&
+		app.Config.Namecheap.API.Token != "" {
+		fmt.Println(Gray("[Config]"), "Namecheap Domain Provider API configuration found, initializing an API connection to the domain provider...")
+		nc := namecheap.NamecheapProvider{
+			Config: namecheap.Config{
+				Debug:    app.Config.Debug,
+				Username: app.Config.Namecheap.Username,
+				API: namecheap.API{
+					Username: app.Config.Namecheap.API.Username,
+					Token:    app.Config.Namecheap.API.Token,
+				},
+			},
+		}
 
-	fmt.Println("Loaded JSON Configuration: ", config)
-	fmt.Println("Where is the false?")
-	fmt.Println("Config.NamecheapConfig: ", config.NamecheapConfig)
-	fmt.Println("Config.NamecheapConfig.Username: ", config.NamecheapConfig.Username)
-	fmt.Println("Config.NamecheapConfig.Password: ", config.NamecheapConfig.Password)
-
-	// TODO: Example usage of the domain provider interface
-	//nc := NamecheapProvider{
-	//	Config: {
-	//		username:    "kosmosblack",
-	//		apiToken:    "",
-	//		apiUsername: "",
-	//	},
-	//}
+		nc.InitializeAPI()
+		fmt.Println(Green("[Domains:Namecheap:API]"), "API connection successfully Established.")
+	} else {
+		fmt.Println(Red("[Fatal Error]"), Gray("No domain provider configuration found, modify the loaded configuration so it contains the API information needed to connect to the domain provider API."))
+	}
 }
